ab3476eef1885170d1644767f9b97dd35c4bea57
matheus <matheus.sa.alves@outlook.com>
2022-05-16 09:37:37 -0300

    finalizando botão de seleção

--- src/components/CheckButoon/index.jsx
+++ src/components/CheckButoon/index.jsx
@@ -3,9 +3,31 @@ import './check-button.css';
 import entrada from '../../images/entradas.svg';
 import saida from '../../images/saidas.svg';
 
-const CheckButton = ({ text, positive }) => {
+const CheckButton = ({ text, positive, onClick, seleceted }) => {
+  const color = ({ clickEntrada, clickSaida }) => {
+    if (clickEntrada) {
+      return '#12A454';
+    }
+    if (clickSaida) {
+      return '#E62E4D';
+    } else {
+      return '';
+    }
+  };
+
   return (
-    <button className='check' type='button'>
+    <button
+      className='check'
+      type='button'
+      onClick={onClick}
+      // style={
+      //   clickEntrada
+      //     ? { background: '#12A454', color: '#FFFFFF' }
+      //     : clickSaida
+      //     ? { background: '#E62E4D', color: '#FFFFFF' }
+      //     : {}
+      // }
+    >
       <img src={positive ? entrada : saida} alt='' />
       <span>{text}</span>
     </button>
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import './modal.css';
 import ButtonRegister from '../ButtonRegister';
 import CheckButton from '../CheckButoon';
-import vector from '../../images/vector.svg'
+import vector from '../../images/vector.svg';
 
 const Modal = () => {
   return (
     <div className='background-modal'>
       <div className='modal'>
+        {/* <img src={vector} alt='Botão de fechar para sair do modal' /> */}
         <h2>Cadastrar transação</h2>
         <input type='text' placeholder='Nome' />
         <input type='number' placeholder='R$' />
         <div className='bt-conteiner'>
-          <CheckButton text='Entrada' positive />
+          <CheckButton
+            text='Entrada'
+            positive
+            onClick={() => console.log('Entrada')}
+          />
           <span></span>
-          <CheckButton text='Saída' />
+          <CheckButton text='Saída' onClick={() => console.log('Saída')} />
         </div>
         <input type='text' placeholder='Categoria' />
         <ButtonRegister />
